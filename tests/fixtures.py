baf453e13027ba9c2fd10280be7ea433b122c474
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-05-21 12:36:52 +0300

    Add admin_client fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,11 +1,11 @@
 import pytest
 
+from rest_framework.test import APIClient, APIRequestFactory
+
 
 @pytest.fixture()
 def rf():
     """RequestFactory instance"""
-
-    from rest_framework.test import APIRequestFactory
 
     return APIRequestFactory()
 
@@ -14,6 +14,13 @@ def rf():
 def client():
     """A Django test client instance."""
 
-    from rest_framework.test import APIClient
-
     return APIClient()
+
+
+@pytest.fixture()
+def admin_client(db, admin_user):
+    """A Django test client logged in as an admin user."""
+
+    client = APIClient()
+    client.login(username=admin_user.username, password='password')
+    return client
