8ba9e2135cc2c9cba481a74d67ce3e5e024746df
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-05-21 11:38:53 +0300

    Refactor serializer User fields

--- linkanywhere/apps/users/serializers.py
+++ linkanywhere/apps/users/serializers.py
@@ -12,8 +12,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password',
-                  'first_name', 'last_name')
+        fields = (
+            'id',
+            'username',
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+        )
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
