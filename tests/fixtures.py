e48dfd94dff24e8991615d1887c553c5f1cb4eda
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-05-23 17:29:22 +0300

    Add login method with mocked authenticate into client fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,3 +1,4 @@
+import mock
 import pytest
 
 from rest_framework.test import APIClient, APIRequestFactory
@@ -14,7 +15,18 @@ def rf():
 def client():
     """A Django test client instance."""
 
-    return APIClient()
+    class _Client(APIClient):
+
+        def login(self, user=None, backend='django.contrib.auth.backends.ModelBackend', **credentials):
+            if user is None:
+                return super().login(**credentials)
+
+            with mock.patch('django.contrib.auth.authenticate') as authenticate:
+                user.backend = backend
+                authenticate.return_value = user
+                return super().login(**credentials)
+
+    return _Client()
 
 
 @pytest.fixture()
