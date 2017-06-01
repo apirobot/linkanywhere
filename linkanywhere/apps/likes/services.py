71d7f83da5508098c3c5b457d622a1b690f7ffce
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-06-01 13:13:30 +0300

    Add get_liked function to the likes services

--- linkanywhere/apps/likes/services.py
+++ linkanywhere/apps/likes/services.py
@@ -1,19 +1,18 @@
-from django.db import models
 from django.db.transaction import atomic
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 
-from linkanywhere.apps.users.models import User
 from .models import Like
 
 
-def add_like(obj: models.Model, user: User) -> Like:
+def add_like(obj, user):
     """Add a like to an object.
 
     If the user has already liked the object nothing happens,
     so this function can be considered idempotent.
 
     :param obj: Any Django model instance.
-    :param user: User adding the like.
+    :param user: User adding the like. :class:`~linkanywhere.apps.users.models.User` instance.
     """
     obj_type = ContentType.objects.get_for_model(obj)
     with atomic():
@@ -24,14 +23,14 @@ def add_like(obj: models.Model, user: User) -> Like:
     return like
 
 
-def remove_like(obj: models.Model, user: User) -> None:
+def remove_like(obj, user):
     """Remove an user like from an object.
 
     If the user has not liked the object nothing happens,
     so this function can be considered idempotent.
 
     :param obj: Any Django model instance.
-    :param user: User removing his like.
+    :param user: User removing his like. :class:`~linkanywhere.apps.users.models.User` instance.
     """
     obj_type = ContentType.objects.get_for_model(obj)
     with atomic():
@@ -40,3 +39,22 @@ def remove_like(obj: models.Model, user: User) -> None:
         if not qs.exists():
             return
         qs.delete()
+
+
+def get_liked(model, user_or_id):
+    """Get the objects liked by an user.
+
+    :param user_or_id: :class:`~linkanywhere.apps.users.models.User` instance or id.
+    :param model: Show only objects of this kind. Can be any Django model class.
+
+    :return: Queryset of objects representing the likes of the user.
+    """
+    obj_type = ContentType.objects.get_for_model(model)
+
+    if isinstance(user_or_id, get_user_model()):
+        user_id = user_or_id.id
+    else:
+        user_id = user_or_id
+
+    return model.objects.filter(likes__user_id=user_id,
+                                likes__content_type=obj_type)
--- tests/likes/test_services.py
+++ tests/likes/test_services.py
@@ -2,7 +2,8 @@ import pytest
 from nose.tools import eq_
 
 from linkanywhere.apps.likes.models import Like
-from linkanywhere.apps.likes.services import add_like, remove_like
+from linkanywhere.apps.likes.services import add_like, remove_like, get_liked
+from linkanywhere.apps.links.models import Link
 from tests.models import LikeModel
 from .. import factories as f
 
@@ -22,3 +23,11 @@ def test_add_and_remove_like():
 
     remove_like(obj, user)
     eq_(Like.objects.count(), 0)
+
+
+def test_get_liked(client):
+    user_1 = f.UserFactory.create()
+    f.LikeLinkFactory(user=user_1)
+    f.LikeLinkFactory(user=user_1)
+
+    eq_(len(get_liked(Link, user_1)), 2)
